c5994bcc0ad44ef2436574fe3650f7eba749d50c
fyang1024 <fyang1024@gmail.com>
2022-02-26 17:50:09 +1100

    Complete lesson 5

---
+++ 05.Lesson_GettingFamiliarWithCVT/ScriptExercise2/MeetingSchedulerRuleMethod.sh
@@ -0,0 +1,5 @@
+certoraRun MeetingSchedulerFixed.sol:MeetingScheduler --verify MeetingScheduler:meetings.spec \
+--solc solc8.7 \
+--rule "startOnTime" \
+--method "startMeeting(uint256)"
+--msg "$1"
