00353a311769af6c8543f662ec7607247fc983b2
fyang1024 <fyang1024@gmail.com>
2022-03-04 11:55:20 +1100

    Complete lesson 11

--- 11.Lesson_Loops/BankLoops/verifyBankWithLoops.sh
+++ 11.Lesson_Loops/BankLoops/verifyBankWithLoops.sh
@@ -1,4 +1,6 @@
 certoraRun BankWithLoops.sol:Bank --verify Bank:Loops.spec \
 --solc solc7.6 \
 --send_only \
+--optimistic_loop \
+--loop_iter 3 \
 --msg "$1"
--- 11.Lesson_Loops/LoopsExample/verifyLoops.sh
+++ 11.Lesson_Loops/LoopsExample/verifyLoops.sh
@@ -1,4 +1,5 @@
 certoraRun Loops.sol:Loops --verify Loops:LoopsUnrolling.spec \
 --solc solc8.11 \
+--loop_iter 10 \
 --send_only \
 --msg "$1"
